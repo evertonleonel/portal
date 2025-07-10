a2f191f6e5dd7eeb32d4eae6d0cbdb283e26befa
everton-leonel <everton.leonel@squadra.com.br>
2025-07-10 17:35:36 -0300

    add: responsividade sidebar

--- src/components/ux/_layouts/auth-layout/index.tsx
+++ src/components/ux/_layouts/auth-layout/index.tsx
@@ -1,5 +1,6 @@
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarProvider, SidebarTrigger, useSidebar } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/ux/app-sidebar';
+import { cn } from '@/lib/utils';
 
 import { Footer } from '../footer';
 import { Header } from '../header';
@@ -8,17 +9,35 @@ export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col">
       <SidebarProvider>
-        <AppSidebar />
-        <div className="relative flex flex-1 flex-col">
-          <Header>
-            <SidebarTrigger className={`top-14 min-[480px]:hidden`} />
-          </Header>
-          <main className="relative flex-1 px-2 sm:p-4 md:p-6 lg:px-10 lg:py-8">
-            {children}
-          </main>
-          <Footer />
-        </div>
+        <AuthLayoutContent children={children} />
       </SidebarProvider>
     </div>
+  );
+};
+
+export const AuthLayoutContent = ({ children }: { children: React.ReactNode }) => {
+  const { state, isMobile } = useSidebar();
+  const sideBarIsExpanded = state === 'expanded';
+  const sideBarIsCollapsed = state === 'collapsed';
+  return (
+    <>
+      <AppSidebar />
+      <div className="relative flex flex-1 flex-col">
+        <Header>
+          <SidebarTrigger className={`top-14 min-[480px]:hidden`} />
+        </Header>
+        <main
+          className={cn(
+            'relative h-full max-h-[calc(100vh-6.75rem)] flex-1 overflow-auto p-2 sm:p-4 md:p-6 lg:px-10 lg:py-8',
+            sideBarIsExpanded && 'max-w-[calc(100vw-16rem)]',
+            sideBarIsCollapsed && 'max-w-[calc(100vw-4.5rem)]',
+            isMobile && 'max-w-[100vw]'
+          )}
+        >
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
