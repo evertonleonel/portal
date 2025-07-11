891f565df9a6027e23493b01e2d9e8c48e38e56c
Everton Leonel <everton_leonel@hotmail.com>
2025-07-10 23:36:37 -0400

    style: melhora estilos de solicitar cadastro

--- src/pages/user-approval-requests/page.tsx
+++ src/pages/user-approval-requests/page.tsx
@@ -1,12 +1,21 @@
 import { Icon } from '@/components/ui/icon';
 import { InputIcon } from '@/components/ui/input-icon';
 import { Label } from '@/components/ui/label';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import { TableApprovals } from './_components/table-approvals';
 import { TableRequests } from './_components/table-requests';
-import { UserApprovalRequestsProvider, useUserApprovalRequestsContext } from './context';
+import {
+  UserApprovalRequestsProvider,
+  useUserApprovalRequestsContext,
+} from './context';
 
 export default function UserApprovalRequests() {
   return (
@@ -27,27 +36,45 @@ const UserApprovalRequestsContent = () => {
       </section>
       <section className="h-full w-full">
         <Tabs defaultValue="solicitacoes">
-          <div className="grid w-full items-center justify-between gap-4 md:flex md:border-separate md:gap-0 md:border-b">
-            <TabsList variant={'ghost'} className="pt-5 max-sm:border-separate max-sm:border-b">
-              <TabsTrigger variant={'ghost'} value="solicitacoes" className="mt-auto h-14 w-full pb-0 font-semibold">
+          <div className="md:border-table grid w-full items-center justify-between gap-4 md:flex md:gap-0 md:border-b">
+            <TabsList
+              variant={'ghost'}
+              className="max-md:border-table pt-5 max-md:border-b"
+            >
+              <TabsTrigger
+                variant={'ghost'}
+                value="solicitacoes"
+                className="mt-auto h-14 w-full pb-0 font-semibold"
+              >
                 <Icon name="profileTickUser" className="size-6" />
                 Solicitações
               </TabsTrigger>
-              <TabsTrigger variant={'ghost'} value="aprovacoes" className="mt-auto h-14 w-full pb-0 font-semibold">
+              <TabsTrigger
+                variant={'ghost'}
+                value="aprovacoes"
+                className="mt-auto h-14 w-full pb-0 font-semibold"
+              >
                 <Icon name="profileUser" className="size-6" />
                 Aprovações
               </TabsTrigger>
             </TabsList>
-            <div className="flex w-full items-center gap-4 py-1.5">
+            <div className="flex w-full items-center gap-4 px-5 py-1.5 lg:px-0">
               <div className="ml-auto flex w-full max-w-40 flex-col gap-2">
-                <Label className="font-manrope text-xs font-normal">Pesquisa por usuário</Label>
+                <Label className="font-manrope text-xs font-normal">
+                  Pesquisa por usuário
+                </Label>
                 <InputIcon
-                  icon={<Icon name="inputSearch" className="text-muted-foreground size-4" />}
+                  icon={
+                    <Icon
+                      name="inputSearch"
+                      className="text-muted-foreground size-4"
+                    />
+                  }
                   placeholder="Pesquisar"
                   className="h-10"
                 />
               </div>
-              <div className="flex min-w-40 flex-col gap-2">
+              <div className="flex min-w-40 flex-col gap-2 self-end">
                 <Label className="font-manrope text-xs font-normal">
                   <Icon name="filter" />
                   Filtrar por empresa
@@ -72,7 +99,10 @@ const UserApprovalRequestsContent = () => {
                       </SelectItem>
                     ) : (
                       empresas.map(empresa => (
-                        <SelectItem key={empresa.id} value={empresa.id.toString()}>
+                        <SelectItem
+                          key={empresa.id}
+                          value={empresa.id.toString()}
+                        >
                           {empresa.nome}
                         </SelectItem>
                       ))
