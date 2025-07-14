cb9715f66c821f85425da175a2d19539b6a24c0c
Everton Leonel <everton_leonel@hotmail.com>
2025-07-14 08:14:12 -0400

    refactora o fetch

--- src/pages/user-approval-requests/_components/hook/use-table-approval-requests.ts
+++ src/pages/user-approval-requests/_components/hook/use-table-approval-requests.ts
@@ -1,65 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { useDebounce } from '@/hooks/use-debounce';
 import { useUserApprovalRequestsContext } from '@/pages/user-approval-requests/context';
 import { getAllUserRequests } from '@/services/user/requests';
 import type { GetUserRequestsResponse } from '@/types/user/requests';
-
 export const useTableApprovalRequests = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [dataRequests, setDataRequests] = useState<GetUserRequestsResponse[]>(
     []
   );
+
   const { filterEmpresaId, filterNome } = useUserApprovalRequestsContext();
 
-  const debounceNome = useDebounce(filterNome, 600);
+  const debouncedNome = useDebounce(filterNome, 600);
+
+  const fetchUserRequests = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const params: {
+        nome?: string;
+        idEmpresa?: number;
+      } = {};
+
+      if (debouncedNome) params.nome = debouncedNome;
+      if (filterEmpresaId) params.idEmpresa = Number(filterEmpresaId);
+
+      const data = await getAllUserRequests(params);
+
+      setDataRequests(data);
+    } catch (error) {
+      console.error(
+        'Erro ao buscar solicitações de cadastro de usuário:',
+        error
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  }, [debouncedNome, filterEmpresaId]);
+
+  useEffect(() => {
+    fetchUserRequests();
+  }, [fetchUserRequests]);
 
   const dataApprovals = dataRequests.filter(el => el.dataAlteracao !== null);
-
-  useEffect(() => {
-    async function fetchUserRequests() {
-      try {
-        setIsLoading(true);
-
-        const data = await getAllUserRequests({});
-
-        setDataRequests(data);
-      } catch (error) {
-        console.error(
-          'Erro ao buscar solicitações de cadastro de usuário:',
-          error
-        );
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    if (!filterNome && !filterEmpresaId) {
-      fetchUserRequests();
-    }
-  }, []);
-
-  useEffect(() => {
-    async function fetchUserRequests() {
-      try {
-        setIsLoading(true);
-        const data = await getAllUserRequests({
-          nome: debounceNome,
-          idEmpresa: Number(filterEmpresaId),
-        });
-        setDataRequests(data);
-      } catch (error) {
-        console.error(
-          'Erro ao buscar solicitações de cadastro de usuário:',
-          error
-        );
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchUserRequests();
-  }, [debounceNome, filterEmpresaId]);
 
   return { dataApprovals, dataRequests, isLoading };
 };
