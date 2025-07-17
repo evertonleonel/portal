e0db840f84165f4260c012ea1b17a16d0c5a6e30
Everton Leonel <everton_leonel@hotmail.com>
2025-07-16 23:13:43 -0400

    remove: modal antigo

--- src/utils/format-date-with-hour/index.ts
+++ src/utils/format-date-with-hour/index.ts
@@ -3,5 +3,5 @@ import { ptBR } from 'date-fns/locale';
 
 export function formatDateWithHour(date: string | Date) {
   return format(new Date(date), "dd/MM/yyyy HH:mm'h'", { locale: ptBR });
-  //saida 10/01/2020 15:30h
+  // => 30/12/2020 15:30h
 }
